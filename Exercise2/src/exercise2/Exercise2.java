62f358b848e8b4ea2a5ea0214018aacda42948de
ruben.carrasco <ruben.carrasco@106-05.info2>
2024-09-20 14:03:13 +0200

    Exercise2 terminado

---
+++ Exercise2/src/exercise2/Exercise2.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, how are you ");
+		System.out.println("Fine thanks.");
+		//println es en nueva línea
+	}
+	
+}
